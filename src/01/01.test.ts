93b838c9c26cd957b61f72e7ccc47f9f94a80de8
Alexandr_P <apalion97@gmail.com>
2021-10-13 16:50:11 +0300

    ad test

---
+++ src/01/01.test.ts
@@ -0,0 +1,37 @@
+import {sum} from "./01";
+import {mult} from "./01";
+
+let a:number;
+let b:number;
+let c:number;
+
+beforeEach(() => {
+  a = 1;
+  b = 2;
+  c = 3;
+})
+
+test('sum should be correct', () => {
+    //action
+    let result1 = sum(a, b);
+    let result2 = sum(a, c);
+    let result3 = sum(b, c);
+
+    //expect result
+    expect(result1).toBe(3)
+    expect(result2).toBe(4)
+    expect(result3).toBe(5)
+})
+
+test('multiply should be correct', () => {
+
+    //action
+    let result1 = mult(a, b);
+    let result2 = mult(a, c);
+    let result3 = mult(b, c);
+
+    //expect result
+    expect(result1).toBe(2)
+    expect(result2).toBe(3)
+    expect(result3).toBe(6)
+})
